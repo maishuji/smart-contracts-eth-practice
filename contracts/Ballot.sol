6e83385491fcdffdd8dadb97efd002cf3b0d8fd4
maishuji <daoliangshumaishuji@gmail.com>
2025-01-03 14:08:19 +0100

    Add stages to Ballot

--- contracts/Ballot.sol
+++ contracts/Ballot.sol
@@ -12,9 +12,14 @@ contract Ballot{
         uint voteCount;
     }
 
+    enum Stage {Init, Reg, Vote, Done}
+    Stage public stage = Stage.Init;
+
     address chairPerson;
     mapping (address => Voter) voters;
     Proposal[] proposals;
+
+    uint startTime;
 
     // Construct a Ballot with n proposals
     constructor(uint8 _numProposals) {
@@ -27,16 +32,28 @@ contract Ballot{
         for(uint idxProp = 0; idxProp < _numProposals; ++idxProp ){
             proposals.push(zeroProp);
         }
+        // Now move fron Init stage to Register stage
+        stage = Stage.Reg;
+        startTime = block.timestamp;
     }
 
     // Must be register by chairPerson
     function register(address toVoter) public {
+        // Check we are in Registration stage
+        require(stage == Stage.Reg, "Cannot register during the current stage");
+
         if(msg.sender != chairPerson || voters[toVoter].voted)return;
         voters[toVoter].weight = 1;
         voters[toVoter].voted = false;
+
+        if(block.timestamp > (startTime + 10 seconds)) {
+            stage = Stage.Vote;
+            startTime = block.timestamp;
+        }
     }
 
     function vote(uint8 toProposal) public {
+        require(stage == Stage.Vote, "Cannot vote during the current stage");
         // storage keyword : Data stored persistently on the Eth. blockchain
         // stored in contract's storage
         // Expensive to read/write because it involves operations on the blockchain
@@ -48,6 +65,7 @@ contract Ballot{
     }
 
     function winningProposal() public view returns (uint _winningProposal) {
+        require(stage == Stage.Done, "Cannot see the result before the vote has ended");
         uint256 winningVoteCount = 0;
         for (uint8 prop = 0; prop < proposals.length; ++prop) {
             if( proposals[prop].voteCount > winningVoteCount) {
