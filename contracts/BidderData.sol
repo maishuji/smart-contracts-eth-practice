93ea72346aef34021b201b99eb4787df861d6235
maishuji <daoliangshumaishuji@gmail.com>
2025-01-03 14:45:51 +0100

    Add Bidder

---
+++ contracts/BidderData.sol
@@ -0,0 +1,26 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22; 
+
+contract Bidder {
+    
+     string public name = "Buffalo";
+     uint public bidAmount;
+     bool public  eligible;
+     uint constant minBid = 1000;
+     
+     function setName(string memory nm) public {
+        name = nm;
+        
+    }
+    
+    function setBidAmount(uint x) public {
+       bidAmount  = x;
+    }
+  
+    function determineEligibility() public {
+        if (bidAmount >= minBid ) eligible = true;
+        else eligible = false;
+
+        
+    }
+}
